25ecb7a7ac483438b192640eaa0fc4c961eb1e7c
Colin Dugeai <colin.dugeai@gmail.com>
2025-07-21 14:24:22 +0200

    feat: create PointGeo

--- traceplot/src/traceplot/types/__init__.py
+++ traceplot/src/traceplot/types/__init__.py
@@ -1,4 +1,15 @@
 Point = tuple[float, float]
+
+
+class PointGeo:
+    lng: float
+    lat: float
+
+    def __init__(self, lng: float, lat: float) -> None:
+        self.lng = lng
+        self.lat = lat
+
+
 ## todo fix
 Segment = tuple[float, float]
 ZoomLevel = int
