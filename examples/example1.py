919f5e44970dc13ea597f03053dd828f8a67e266
Colin Dugeai <colin.dugeai@gmail.com>
2025-07-21 15:32:47 +0200

    chore: example updated

--- examples/example1.py
+++ examples/example1.py
@@ -1,12 +1,45 @@
-from traceplot import hello
 from traceplot.helpers import greet
+from traceplot.helpers.gmaps import generate_map_png
+from traceplot import figure_map
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()  # take environment variables
 
 
-def main():
-    print("Hello from examples!")
-    greet.greetme()
-    print(hello())
+api_key = os.environ.get('GMAPS_API_KEY')
 
 
-if __name__ == "__main__":
-    main()
+FIG_PATH = "out/background_paris.png"
+
+# Generate background image
+bbox_png = generate_map_png(
+    maptype="roadmap",
+    lat=48.8589385,
+    lon=2.2646338,
+    width_px_sat=640,
+    height_px_sat=640,
+    radius_meters=10000,
+    figure_path=FIG_PATH,
+    gmaps_api_key=api_key,
+)
+
+# Add track to image
+figure_map(
+    [
+        (2.282259089265323, 48.88677456721743, 50),
+        (2.2746338, 48.8589385, 55),
+        (2.3850533122051463, 48.82797239213328, 60),
+    ],
+    bbox_png,
+    FIG_PATH,
+    "out/trace_paris1.png",
+    "My first stage",
+    "From here",
+    "to here",
+    "img/marker_start.png",
+    "img/marker_finish.png",
+)
+
+print("map done")
